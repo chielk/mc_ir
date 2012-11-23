60935dcb1835436176f56df5e6c301bafb316f03
Michael Cabot <michael.a.cabot@gmail.com>
2012-11-23 12:04:35 +0100

    add missing bracket to thesaurus. removed extra prob from expandQuery

--- assignment_2/expandQuery.py
+++ assignment_2/expandQuery.py
@@ -7,12 +7,12 @@ def expand(inputFileName, outputFileName, thesaurus):
     synProb = 0.5
     for tag in treeRoot.findall('query'): # for each query
         query = tag.find('text')
-        query.text += " #wsyn( " + str(synProb) + " "
+        query.text += " #wsyn( " # weighted synonyms
         for word in query.text.split(): # for each word in query
             for syn in thesaurus.get(word): # find synonyms
                 query.text += " %.1f %s " % (synProb, syn)
             
-        query.text += ")"
+        query.text += ")" # end weighted synonyms
     tree.write(outputFileName)
 
     
