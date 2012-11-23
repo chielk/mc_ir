cfdeb1d2a1aed9b52c3b9862fd250096578d5b15
Michael Cabot <michael.a.cabot@gmail.com>
2012-11-23 11:49:58 +0100

    get each syn 1 at a time

--- assignment_2/expandQuery.py
+++ assignment_2/expandQuery.py
@@ -1,18 +1,21 @@
 import xml.etree.ElementTree as ET
 from thesaurus import Thesaurus
 
-def expand(inputFileName, outputFileName, theSaurus):
+def expand(inputFileName, outputFileName, thesaurus):
     tree = ET.parse(inputFileName)
     treeRoot = tree.getroot()
-    for tag in treeRoot.findall('query'):
+    synProb = 0.5
+    for tag in treeRoot.findall('query'): # for each query
         query = tag.find('text')
-        query.text += " #wsyn( 0.5 "
-        for word in query.text.split():
-            for synList in theSaurus.get(word):
-                query.text += ' 0.5 '.join(synList)
+        query.text += " #wsyn( " + str(synProb) + " "
+        for word in query.text.split(): # for each word in query
+            for syn in thesaurus.get(word): # find synonyms
+                query.text += " %.1f %s " % (synProb, syn)
+            
         query.text += ")"
     tree.write(outputFileName)
-        
+
+    
 if __name__ == "__main__":
     ts = Thesaurus()
     expand("topics-indri-qe.trec", "testQE.trec", ts)
