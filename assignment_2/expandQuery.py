b5988f3a003fbb773e595bd109b2c477f0b66f56
Michael Cabot <michael.a.cabot@gmail.com>
2012-11-29 13:14:14 +0100

    expandQuery runs indri and trec_eval for different synonym weights

--- assignment_2/expandQuery.py
+++ assignment_2/expandQuery.py
@@ -1,21 +1,42 @@
 import xml.etree.ElementTree as ET
+import subprocess
 from thesaurus import Thesaurus
 
-def expand(inputFileName, outputFileName, thesaurus):
+def main():
+    ts = Thesaurus()
+    originalQueryFile = "topics-indri-qe.trec"
+    trecRelFile = "document-qrels.txt"
+    for x in xrange(6):
+        synWeight = x/10.0
+        print "synWeight: %.1f" %synWeight
+        # expand queries
+        print "expand queries"
+        outputQueryFile = "testQE%d.trec" %x
+        expand(originalQueryFile, outputQueryFile, ts, synWeight)
+        # run queries
+        print "run queries"
+        queryOutputFile = "output_synW%d.txt" %x
+        queryProcess = "IndriRunQuery %s > %s" %(outputQueryFile, queryOutputFile)
+        subprocess.call(queryProcess, shell=True)
+        # evaluate result
+        print "evaluate result"
+        resultFile = "result_synW%d.txt" %x
+        trecEvalProcess = "trec_eval %s %s > %s" %(trecRelFile, queryOutputFile, resultFile)
+        subprocess.call(trecEvalProcess, shell=True)
+
+def expand(inputFileName, outputFileName, thesaurus, synWeight):
     tree = ET.parse(inputFileName)
     treeRoot = tree.getroot()
-    synProb = 0.5
     for tag in treeRoot.findall('query'): # for each query
         query = tag.find('text')
         query.text += " #wsyn( " # weighted synonyms
         for word in query.text.split(): # for each word in query
             for syn in thesaurus.get(word): # find synonyms
-                query.text += " %.1f %s " % (synProb, syn)
+                query.text += " %.1f %s " % (synWeight, syn)
             
         query.text += ")" # end weighted synonyms
     tree.write(outputFileName)
 
     
 if __name__ == "__main__":
-    ts = Thesaurus()
-    expand("topics-indri-qe.trec", "testQE.trec", ts)
+    main()
